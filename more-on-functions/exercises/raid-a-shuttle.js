899876bcdeeea4f05c8dd620eb48de88269e8ac1
AnaHolford <anabeatrizjpms@gmail.com>
2024-06-04 19:50:23 -0500

    more on functions exercises

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -1,28 +1,57 @@
+let fuelLevel = 200000;
+let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
+
 function checkFuel(level) {
-  if (level > 100000){
+  if (level > 100000) {
     return 'green';
-  } else if (level > 50000){
+  } else if (level > 50000) {
     return 'yellow';
   } else {
     return 'red';
   }
 }
 
-function holdStatus(arr){
+function holdStatus(arr) {
   if (arr.length < 7) {
     return `Spaces available: ${7-arr.length}.`;
-  } else if (arr.length > 7){
+  } else if (arr.length > 7) {
     return `Over capacity by ${arr.length-7} items.`;
   } else {
     return "Full";
   }
 }
 
-let fuelLevel = 200000;
-let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
+let normalVariable = function (a) {
+  if (checkFuel(a) === 'green') {
+    return a - 100001
+  }
+  if (checkFuel(a) === 'yellow') {
+    return a - 50001
+  } else return a
+}
+
+let innocentVariable = function (myArray) {
+  let item1 = ''
+  let item2 = ''
+
+  item1 = myArray.splice(1, 1, 'orange')
+  item2 = myArray.splice(4, 1, 'banana')
+  
+  return item1.concat(item2)
+}
+
+//console.log(innocentVariable(cargoHold))
 
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
+
+let irs = function (levelOfFuel, holdOfCargo) {
+let arr = holdOfCargo(cargoHold)
+return `Raided ${levelOfFuel(fuelLevel)} kg of fuel from the tanks, and stole ${arr[0]} and ${arr[1]} from the cargo hold.`
+
+}  
+
+console.log(irs(normalVariable,innocentVariable));
 
 /* Steal some fuel from the shuttle:
  * /
@@ -36,7 +65,8 @@ console.log("Hold status: " + holdStatus(cargoHold));
 //d). Decide where to best place your function call to gather our new fuel.
 
 /* Next, liberate some of that glorious cargo.
- * /
+ */
+
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
@@ -47,11 +77,12 @@ console.log("Hold status: " + holdStatus(cargoHold));
 //d). Don’t get hasty, matey! Remember to test your function.
 
 /* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
- 
+ */
+
+
+
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
-	
+
 //b). Call your anonymous fuel and cargo functions from within irs.
 
 //c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
-
